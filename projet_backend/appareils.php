ba973188c388de723722ccfc1fd5f13d50142020
Adrien Dupont <95227652+Adrienfdupont@users.noreply.github.com>
2022-05-04 15:12:04 +0200

    Add files via upload

---
+++ projet_backend/appareils.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mes appareils</title>
+    <link rel="stylesheet" href="./sources_front/appareils.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
